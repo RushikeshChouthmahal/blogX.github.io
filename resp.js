10871742f2a3a6b0e8c704c9dcff69a7dc37c3be
Rushikesh <Rushi@example.com>
2023-08-23 20:15:19 +0530

    fixing txt and add title

--- resp.js
+++ resp.js
@@ -4,10 +4,11 @@ burger = document.querySelector(".burger")
 navbar = document.querySelector(".responsive-h-nav")
 leftnav = document.querySelector(".leftnav")
 rightnav = document.querySelector(".rightnav")
+blogx = document.querySelector(".container")
 
 burger.addEventListener("click", ()=>{
     leftnav.classList.toggle("responsive-opt");
     rightnav.classList.toggle("responsive-opt");
     navbar.classList.toggle("responsive-h-nav");
-
+    blogx.classList.toggle("remove");
 })
